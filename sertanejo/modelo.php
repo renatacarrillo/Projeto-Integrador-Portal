e2f7d438180869aaff656b2e29fa2835e81dedd7
Reginaldo C Domingos <39439355+rcdomingos@users.noreply.github.com>
2018-10-04 10:28:23 -0300

    Add pagina modelo

---
+++ sertanejo/modelo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <!--importando o head da pagina -->
+  <?php
+    include('../imports/head.php');
+  ?>
+  <body>
+    <!--importandoo menu da pagina -->
+    <?php
+      include('../imports/menu-secundario.php');
+    ?>
+    <!-- conteudo da pagina -->
+
+
+    Seu Conteudo
+
+
+
+    <!-- importando o footer da pagina -->
+    <?php
+      include('../imports/footer-secundario.php');
+    ?>
+  </body>
+</html>
