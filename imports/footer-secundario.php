3429577b4d970dd5211fda74e23566f79daaa18d
Reginaldo C Domingos <39439355+rcdomingos@users.noreply.github.com>
2018-10-04 10:29:00 -0300

    add footer secundario

---
+++ imports/footer-secundario.php
@@ -0,0 +1,94 @@
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-1">
+          <p>SERVIÇOS</p>
+        </div>
+        <div class="col-md-3">
+          <nav class="menu-secundario">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Sobre Nós</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Promoções</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Devoluções</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">FAQ</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Seja Um Apoiador</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="col-md-1">
+          <p>MAPA</p>
+        </div>
+        <div class="col-md-3">
+          <nav class="menu-secundario">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Artistas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Noticias</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ranking</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Cifras</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Indicações</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="col-md-1">
+          <p>CONTATO</p>
+        </div>
+        <div class="col-md-3">
+          <nav class="menu-secundario">
+            <ul class="nav flex-column">
+              <li class="nav-item nav-link">
+                + 55 11 1254 5678
+              </li>
+              <li class="nav-item nav-link">
+                contato@ecletic.music.com.br
+              </li>
+              <li class="nav-item nav-link">
+                Sua Opinião nos ajuda!
+              </li>
+            </ul>
+          </nav>
+          <div id="redes-sociais-footer">
+            <ul>
+              <li class="face"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+              <li class="insta"><a href="#"><i class="fab fa-instagram"></i></a></li>
+              <li class="twit"><a href="#"><i class="fab fa-twitter"></i></a></li>
+            </ul>
+          </div>
+          <div class="logo-footer">
+            <img src="../images/home/LOGO.svg" class="d-inline-block" alt="[LOGO] Ecletic.Music">
+          </div>
+        </div>
+      </div>
+      <div class="row copy">
+        <div class="col-12">
+          <p><small>todos os direitos reservado e etc,etc e etc</small> </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- Importanto os scripts JAVA -->
+  <script src="../js/jquery-3.3.1.min.js"></script>
+  <script src="../js/bootstrap.js"> </script>
+  <script src="../js/nosso-js.js"> </script>
