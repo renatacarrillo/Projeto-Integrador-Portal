d48201a2e1a3e037d02db3742626d4ea40425108
Renata Carrillo <39443040+renatacarrillo@users.noreply.github.com>
2018-10-23 22:56:34 -0300

    Add files via upload

---
+++ hip-hop/lancamentos.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<!--importando o head da pagina -->
+  <?php
+    include('../imports/head.php');
+  ?>
+
+<body>
+  <!--importandoo menu da pagina -->
+  <?php
+    include('../imports/menu-secundario.php');
+  ?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-white bg-dark text-center font-italic font-weight-bold">Lançamentos</h1>
+            <main>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                        </div>
+                        <div class="col-sm-2">
+                            <h3>Scorpion</h3>
+                            <h4 class="text-muted">Drake</h4>
+                            <button type="button" class="btn btn-primary btn-lg btn-block btn btn-success float-none">Spotify</button>
+                        </div>
+                        <div class="col-sm-4">
+                            <img src="../images/hip-hop/geay.jpg" class="img-fluid" alt="Responsive image">
+                        </div>
+                            <div class="col-sm-2">
+                            <h3>When It's The Dark</h3>
+                            <h4 class="text-muted">G-Eazy</h4>
+                            <button type="button" class="btn btn-primary btn-lg btn-block btn btn-success float-none">Spotify</button>
+                            </div>
+                        </div>
+
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <img src="../images/hip-hop/zzzz.jpg" class="img-fluid" alt="Responsive image">
+                        </div>
+                        <div class="col-sm-2">
+                            <h3>Beauty Behind The Madness</h3>
+                            <h4 class="text-muted">The Weekends</h4>
+                            <button type="button" class="btn btn-primary btn-lg btn-block btn btn-success float-none">Spotify</button>
+                        </div>
+                        <div class="col-sm-4">
+                            <img src="../images/hip-hop/G-Eazy-album-cover-770x731.png" class="img-fluid" alt="Responsive image">
+                        </div>
+                        <div class="col-sm-2">
+                            <h3>The Beauty and Damned</h3>
+                            <h4 class="text-muted">G-Eazy</h4>
+                            <button type="button" class="btn btn-primary btn-lg btn-block btn btn-success float-none">Spotify</button>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <img src="../images/hip-hop/fff.png" class="img-fluid" alt="Responsive image">
+                        </div>
+                        <div class="col-sm-2">
+                            <h3>Beauty Behind The Madness</h3>
+                            <h4 class="text-muted">The Weekends</h4>
+                            <button type="button" class="btn btn-primary btn-lg btn-block btn btn-success float-none">Spotify</button>
+                        </div>
+                        <div class="col-sm-4">
+                            <img src="../images/hip-hop/ggg.jpg" class="img-fluid" alt="Responsive image">
+                        </div>
+                        <div class="col-sm-2">
+                            <h3>The Beauty and Damned</h3>
+                            <h4 class="text-muted">G-Eazy</h4>
+                            <button type="button" class="btn btn-primary btn-lg btn-block btn btn-success float-none">Spotify</button>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+    </div>
+</body>
+
+<!-- importando o footer da pagina -->
+  <?php
+    include('../imports/footer-secundario.php');
+  ?>
+</html>
---
+++ hip-hop/origem.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<!--importando o head da pagina -->
+<?php
+    include('../imports/head.php');
+  ?>
+
+<main>
+    <!--importandoo menu da pagina -->
+    <?php
+    include('../imports/menu-secundario.php');
+  ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <main>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                    <h1 class="text-white bg-dark text-center font-italic font-weight-bold">Origem</h1>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <img src="../images/hip-hop/drake2.jpg" class="img-fluid" alt="Responsive image">
+                            </div>
+                            <div class="col-sm-8">
+                                <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sollicitudin urna nec vestibulum pellentesque. Ut leo felis, malesuada quis nunc id, ornare aliquam dolor. Cras sed felis pretium ex sollicitudin fermentum. Donec ultricies felis sed consectetur accumsan. Proin libero ante, consequat sit amet justo id, sodales congue ipsum. Sed condimentum ante eget ex imperdiet pretium. Ut pretium nisl lacinia leo ultrices commodo. Curabitur neque lacus, elementum ut nisl et, tristique lobortis mi. Nunc consectetur fermentum erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sollicitudin urna nec vestibulum pellentesque. Ut leo felis, malesuada quis nunc id, ornare aliquam dolor. Cras sed felis pretium ex sollicitudin fermentum. Donec ultricies felis sed consectetur accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sollicitudin urna nec vestibulum pellentesque. Ut leo felis, malesuada quis nunc id, ornare aliquam dolor. Cras sed felis pretium ex sollicitudin fermentum. Donec ultricies felis sed consectetur accumsan.</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <h4 class="text-center">PARCERIAS</h4>
+                                <img src="../images/hip-hop/mac_1.jpg" class="img-fluid" alt="Responsive image">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sollicitudin urna nec vestibulum pellentesque. Ut leo felis, malesuada quis nunc id, ornare aliquam dolor. Cras sed felis pretium ex sollicitudin fermentum. Donec ultricies felis sed consectetur accumsan.</p>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <h4 class="text-center">SHOWS</h4>
+                                <img src="../images/hip-hop/mac_1.jpg" class="img-fluid" alt="Responsive image">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sollicitudin urna nec vestibulum pellentesque. Ut leo felis, malesuada quis nunc id, ornare aliquam dolor. Cras sed felis pretium ex sollicitudin fermentum. Donec ultricies felis sed consectetur accumsan.</p>
+                            </div>
+                            <div class="col-sm-4">
+                                <h4 class="text-center">ACONTECIMENTOS</h4>
+                                <img src="../images/hip-hop/mac_1.jpg" class="img-fluid" alt="Responsive image">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sollicitudin urna nec vestibulum pellentesque. Ut leo felis, malesuada quis nunc id, ornare aliquam dolor. Cras sed felis pretium ex sollicitudin fermentum. Donec ultricies felis sed consectetur accumsan.</p>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    </div>
+    <!-- importando o footer da pagina -->
+    <?php
+    include('../imports/footer-secundario.php');
+  ?>
+</main>
+
+</html>
