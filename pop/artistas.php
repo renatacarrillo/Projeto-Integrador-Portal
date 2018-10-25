9ff214c55eea8d66ffbc5129903d874c25e5fa00
Reginaldo C Domingos <rcdomingos@live.com>
2018-10-24 22:49:46 -0300

    adicionado as paginas POP

---
+++ pop/artistas.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<!--importando o head da pagina -->
+<?php
+    include('../imports/head.php');
+  ?>
+
+<body>
+    <!--importandoo menu da pagina -->
+    <?php
+    include('../imports/menu-secundario.php');
+  ?>
+
+    <!-- Conteudo da pagina  -->
+    <main>
+        <div class="container mt-3">
+            <h1 class="text-center">Artistas em Alta</h1>
+            <div class="row">
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <img class="img-fluid" src="../images/pop/justin.jpg" alt="Justin">
+                </div>
+                <div class="col-6  col-md-3 col-lg-2 col-xl-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6  col-md-3 col-lg-2 col-xl-2">
+                    <img class="img-fluid" src="../images/pop/taylor.jpg" alt="Taylor">
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2 ">
+                    <img class="img-fluid" src="../images/pop/lady%20gaga.jpg" alt="Lady Gaga">
+                </div>
+                <div class="col-6  col-md-3 col-lg-2 col-xl-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6  col-md-3 col-lg-2 col-xl-2">
+                    <img class="img-fluid" src="../images/pop/zayn-malik.jpg" alt="Zayn">
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <img class="img-fluid" src="../images/pop/Rihanna.jpg" alt="Rihanna">
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <p class="espaço-art">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <img class="espaço-art  img-fluid" src="../images/pop/justinberlake.jpg" alt="Justimberlake">
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <p class="espaço-art">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <img class="img-fluid" src="../images/pop/zayn-malik.jpg" alt="Zayn">
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <img class="img-fluid" src="../images/pop/Rihanna.jpg" alt="Rihanna">
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <p class="espaço-art">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <img class="espaço-art  img-fluid" src="../images/pop/justinberlake.jpg" alt="Justimberlake">
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <p class="espaço-art">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <img class="img-fluid" src="../images/pop/justin.jpg" alt="Justin">
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <img class="img-fluid" src="../images/pop/taylor.jpg" alt="Taylor">
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <img class="img-fluid" src="../images/pop/lady%20gaga.jpg" alt="Lady Gaga">
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 col-xl-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id elit diam. </p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- importando o footer da pagina -->
+    <?php
+    include('../imports/footer-secundario.php');
+  ?>
+
+</body>
+
+</html>
---
+++ pop/lancamentos.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<!--importando o head da pagina -->
+<?php
+    include('../imports/head.php');
+  ?>
+
+<body>
+    <!--importandoo menu da pagina -->
+    <?php
+    include('../imports/menu-secundario.php');
+  ?>
+
+    <!-- Conteudo da pagina  -->
+    <main>
+        <div class="container mt-2 mb-3">
+            <div class="row no-gutters">
+                <div class=" mt-1 col-md-6">
+
+                    <div id="carouselpop" class="carousel slide carousel-fade" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="../images/pop/LADY%20SHOW%20-%20Copia.jpg" alt="Primeiro Slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="../images/pop/taylor%20show%20-%20Copia.jpg" alt="Segundo Slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselpop" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Anterior</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselpop" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Próximo</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-1">
+                    <div class="container-fluid">
+
+                        <h2 class="text-center">Lançamentos da Semana</h2>
+                        <ul class="list-group">
+                            <li class="list-group-item">1ª Loremipsum dolorsit ametconsec.</li>
+                            <li class="list-group-item">2º Loremipsum dolorsit ametconsec.</li>
+                            <li class="list-group-item">3º Loremipsum dolorsit ametconsec.</li>
+                            <li class="list-group-item">4º Loremipsum dolorsit ametconsec.</li>
+                            <li class="list-group-item">5º Loremipsum dolorsit ametconsec.</li>
+                            <li class="list-group-item">6º Loremipsum dolorsit ametconsec.</li>
+                            <li class="list-group-item">7º Loremipsum dolorsit ametconsec.</li>
+                            <li class="list-group-item">8º Loremipsum dolorsit ametconsec.</li>
+                            <li class="list-group-item">9º Loremipsum dolorsit ametconsec.</li>
+                            <li class="list-group-item">10º Loremipsum dolorsit ametconsec.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- importando o footer da pagina -->
+    <?php
+    include('../imports/footer-secundario.php');
+  ?>
+</body>
+
+</html>
