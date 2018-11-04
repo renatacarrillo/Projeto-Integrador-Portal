fa8bba3c023a9841114adc228160f12d948b05bf
Thaís Oliveira <39445154+thaismachado94@users.noreply.github.com>
2018-11-04 17:58:41 -0200

    Add files via upload

---
+++ hip-hop/lancamentos.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<!--importando o head da pagina -->
+<?php
+    include('../imports/head.php');
+  ?>
+
+<body>
+    <!--importandoo menu da pagina -->
+    <?php
+    include('../imports/menu-secundario.php');
+  ?>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mt-4 mb-4">
+                    <h1 class="text-white bg-dark text-center font-italic font-weight-bold">Lançamentos</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="container">
+                        <div class="row">
+                <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                </div>
+
+                <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                </div>
+
+                <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                </div>
+                            <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                </div>
+                            <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                </div>
+                            <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                            </div>
+                                
+                                <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                </div>
+                                
+                                <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                            </div>
+                            
+                                                            <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                            </div>
+                            
+                                                            <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                            </div>
+                            
+                                                            <div class="col-6 col-sm-3">
+                    <img src="../images/hip-hop/capadrake.jpg" class="img-fluid" alt="Responsive image">
+                    <h3>Scorpion</h3>
+                    <h4 class="text-muted">Drake</h4>
+                    <button type="button" class="btn btn-outline-success">Spotify</button>
+                            </div>
+                </div>
+                </div>
+                </div>
+                        <div class="col-12 col-sm-4">
+                            <div class="list-group">
+                                <a class="list-group-item list-group-item-action flex-column align-items-start">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h5 class="mb-1">Konai</h5>
+                                        <small class="text-muted">3 dias atrás</small>
+                                    </div>
+                                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                    <small class="text-muted">Donec id elit non mi porta.</small>
+                                </a>
+                                <a class="list-group-item list-group-item-action flex-column align-items-start">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h5 class="mb-1">Blackbear</h5>
+                                        <small class="text-muted">3 dias atrás</small>
+                                    </div>
+                                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                    <small class="text-muted">Donec id elit non mi porta.</small>
+                                </a>
+                                <a class="list-group-item list-group-item-action flex-column align-items-start">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h5 class="mb-1">Tyga</h5>
+                                        <small class="text-muted">3 dias atrás</small>
+                                    </div>
+                                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                    <small class="text-muted">Donec id elit non mi porta.</small>
+                                </a>
+
+                                <a class="list-group-item list-group-item-action flex-column align-items-start">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h5 class="mb-1">Offset</h5>
+                                        <small class="text-muted">3 dias atrás</small>
+                                    </div>
+                                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                    <small class="text-muted">Donec id elit non mi porta.</small>
+                                </a>
+
+                                <a class="list-group-item list-group-item-action flex-column align-items-start">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h5 class="mb-1">Mac Miller</h5>
+                                        <small class="text-muted">3 dias atrás</small>
+                                    </div>
+                                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                    <small class="text-muted">Donec id elit non mi porta.</small>
+                                </a>
+
+                                <a class="list-group-item list-group-item-action flex-column align-items-start">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h5 class="mb-1">Lyl Wayne</h5>
+                                        <small class="text-muted">3 dias atrás</small>
+                                    </div>
+                                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                    <small class="text-muted">Donec id elit non mi porta.</small>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </main>
+</body>
+
+<!-- importando o footer da pagina -->
+<?php
+    include('../imports/footer-secundario.php');
+  ?>
+
+</html>
