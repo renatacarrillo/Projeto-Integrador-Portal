75287f48474107bf510d1d242a19113a8b0b32f7
Karina Cavalcanti Lucindo <43181660+KarinaCavalcanti@users.noreply.github.com>
2018-11-03 22:21:20 -0300

    Add files via upload

--- classica/artistas.php
+++ classica/artistas.php
@@ -14,6 +14,14 @@
     include('../imports/menu-secundario.php');
 ?>
     <!--NAV ENDS-->
+    <section class="artistas-classica">
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4 text-white">Compositores</h1>
+            <p class="lead text-white">Conheça um pouco dos vários compositores talentosos da história da música clássica</p>
+        </div>
+    </div>
+        </section>
 
     <div class="container">
         <!--ARTICLE-->
@@ -23,55 +31,55 @@
                 <div class="col-12 col-md-6 col-lg-4">
                     <img src="../images/classica/artistas-01.jpg" class="img-fluid mt-2 mb-2 rounded imgartistas" alt="beethoven" id="imgone">
                 </div>
-                    <article class="col-12 col-md-6 col-lg-8">
-                        <div>
-                            <h3 class="tartistas text-center">Ludwig Van Beethoven</h3>
-                            <p class="partistas text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet malesuada feugiat. Curabitur fermentum bibendum nulla, teste, non dictum ipsum tincidunt non. Quisque convallis pharetra tempor. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem.</p>
-                        </div>
-                    </article>
-                </div>
-                <div class="row mb-3" id="boxtwo">
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <img src="../images/classica/artistas-02.jpg" class="img-fluid mt-2 mb-2 rounded imgartistas" alt="mozart" id="imgtwo">
+                <article class="col-12 col-md-6 col-lg-8">
+                    <div>
+                        <h3 class="tartistas text-center mt-2">Ludwig Van Beethoven</h3>
+                        <p class="partistas text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet malesuada feugiat. Curabitur fermentum bibendum nulla, teste, non dictum ipsum tincidunt non. Quisque convallis pharetra tempor. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem.</p>
                     </div>
-                    <article class="col-12 col-md-6 col-lg-8">
-                        <div>
-                            <h3 class="tartistas text-center">Wolfgang Amadeus Mozart</h3>
-                            <p class="partistas text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet malesuada feugiat. Curabitur fermentum bibendum nulla, non dictum ipsum tincidunt non. Quisque convallis pharetra tempor. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem.</p>
-                        </div>
-                    </article>
+                </article>
+            </div>
+            <div class="row mb-3" id="boxtwo">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <img src="../images/classica/artistas-02.jpg" class="img-fluid mt-2 mb-2 rounded imgartistas" alt="mozart" id="imgtwo">
                 </div>
-                <div class="row mb-3" id="boxthree">
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <img src="../images/classica/artistas-03.jpg" class="img-fluid mt-2 mb-2 rounded imgartistas" alt="chopin" id="imgthree">
+                <article class="col-12 col-md-6 col-lg-8">
+                    <div>
+                        <h3 class="tartistas text-center mt-2">Wolfgang Amadeus Mozart</h3>
+                        <p class="partistas text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet malesuada feugiat. Curabitur fermentum bibendum nulla, non dictum ipsum tincidunt non. Quisque convallis pharetra tempor. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem.</p>
                     </div>
-                    <article class="col-12 col-md-6 col-lg-8">
-                        <div>
-                            <h3 class="tartistas text-center">Frédéric Chopin</h3>
-                            <p class="partistas text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet malesuada feugiat. Curabitur fermentum bibendum nulla, non dictum ipsum tincidunt non. Quisque convallis pharetra tempor. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem.</p>
-                        </div>
-                    </article>
+                </article>
+            </div>
+            <div class="row mb-3" id="boxthree">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <img src="../images/classica/artistas-03.jpg" class="img-fluid mt-2 mb-2 rounded imgartistas" alt="chopin" id="imgthree">
                 </div>
-                <div class="row mb-3" id="boxfour">
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <img src="../images/classica/artistas-04.jpg" class="img-fluid mt-2 mb-2 rounded imgartistas" alt="tchaikovsky" id="imgfour">
+                <article class="col-12 col-md-6 col-lg-8">
+                    <div>
+                        <h3 class="tartistas text-center mt-2">Frédéric Chopin</h3>
+                        <p class="partistas text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet malesuada feugiat. Curabitur fermentum bibendum nulla, non dictum ipsum tincidunt non. Quisque convallis pharetra tempor. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem.</p>
                     </div>
-                    <article class="col-12 col-md-6 col-lg-8">
-                        <div>
-                            <h3 class="tartistas text-center">Piotr Ilitch Tchaikovsky</h3>
-                            <p class="partistas text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet malesuada feugiat. Curabitur fermentum bibendum nulla, non dictum ipsum tincidunt non. Quisque convallis pharetra tempor. Donec id pretium leo. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem.</p>
-                        </div>
-                    </article>
+                </article>
+            </div>
+            <div class="row mb-3" id="boxfour">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <img src="../images/classica/artistas-04.jpg" class="img-fluid mt-2 mb-2 rounded imgartistas" alt="tchaikovsky" id="imgfour">
                 </div>
+                <article class="col-12 col-md-6 col-lg-8">
+                    <div>
+                        <h3 class="tartistas text-center mt-2">Piotr Ilitch Tchaikovsky</h3>
+                        <p class="partistas text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet malesuada feugiat. Curabitur fermentum bibendum nulla, non dictum ipsum tincidunt non. Quisque convallis pharetra tempor. Donec id pretium leo. Donec id pretium leo. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem. Pellentesque luctus massa non elit viverra pellentesque. Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui. Fusce in urna sem.</p>
+                    </div>
+                </article>
             </div>
         </div>
-        <!--FIM ARTICLE-->
+    </div>
+    <!--FIM ARTICLE-->
 
-        <!--FOOTER-->
-        <?php 
+    <!--FOOTER-->
+    <?php 
     include('../imports/footer-secundario.php');
 ?>
-        <!--FIM FOOTER-->
+    <!--FIM FOOTER-->
 
 </body>
 
